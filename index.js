b2c2e8af46748b813a37ba3daac2137b75882960
varadaraj772 <v.acharya@primesophictech.com>
2024-10-04 12:07:23 +0530

    changes

--- index.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("message", (event) => {
-  const data = JSON.parse(event.data);
-  console.log("Data received from React Native:", data);
-  document.getElementById("mobile").innerHTML = data;
-});
